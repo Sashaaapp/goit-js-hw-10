1c654d9557099ec8b080ec77fd84914b19462dc0
Sashaaapp <sashaaapp1980@gmail.com>
2024-01-13 21:04:51 +0200

    2-shackbar-end

--- src/js/2-shackbar.js
+++ src/js/2-shackbar.js
@@ -2,3 +2,48 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const delayInput = form.querySelector("[name='delay']");
+    const stateInput = form.querySelector("[name='state']:checked");
+
+    const delay = parseInt(delayInput.value);
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (stateInput.value === "fulfilled") {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+
+    promise
+      .then((delay) => {
+
+        iziToast.success({
+
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topRight'
+
+        });
+ 
+      })
+      .catch((delay) => {
+
+        iziToast.error({
+
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topRight'
+
+        });
+
+      });
+  });
+});
